c49ede6921c01edc6e059426d2c76b97e9a6a1b4
digitalman <digitalman@DIGITALMAN-PC>
2016-09-15 00:00:10 +0200

    datepicker bei create events einbauen

--- Meets/App_Start/BundleConfig.cs
+++ Meets/App_Start/BundleConfig.cs
@@ -22,12 +22,14 @@ namespace Meets
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/bootstrap-datepicker.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",                      
                       "~/Content/site.css",
-                      "~/Content/toastr.css"));
+                      "~/Content/toastr.css",
+                      "~/Content/bootstrap-datepicker.css"));
         }
     }
 }
